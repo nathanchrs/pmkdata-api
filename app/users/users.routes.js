2faf57d9dafe12f198231ec0f67fa0a6a8ee1363
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-06-24 20:50:08 +0700

    Added users list endpoint

--- app/users/users.routes.js
+++ app/users/users.routes.js
@@ -1,15 +1,28 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../components/knex.js');
 const auth = require('../components/auth.js');
 const validators = require('./users.validators.js');
 
 const router = express.Router();
 
-/* Get list of users */
-
-router.get('/users', auth.isSupervisor, (req, res, next) => {
-
+/**
+ * Get a list of users.
+ * @name Get users
+ * @route {GET} /users
+ */
+router.get('/users', auth.isSupervisor, validators.listUsers, (req, res, next) => {
+  return knex.select('username', 'nim', 'email', 'role', 'created_at', 'updated_at')
+    .from('users')
+    .search(req.query.search, ['username', 'nim', 'email'])
+    .pageAndSort(req.query.page, req.query.perPage, req.query.sort, ['username', 'nim', 'email', 'role', 'created_at', 'updated_at'])
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((err) => {
+      return next(err);
+    });
 });
 
 module.exports = router;
--- app/users/users.validators.js
+++ app/users/users.validators.js
@@ -5,7 +5,25 @@ const validation = require('../components/validation.js');
 
 const schemas = {
 
-  
+  listUsers: {
+    'type': 'object',
+    'properties': {
+      'search': {
+        'type': 'string'
+      },
+      'page': {
+        'type': 'number',
+        'minimum': 1
+      },
+      'perPage': {
+        'type': 'number',
+        'minimum': 1
+      },
+      'sort': {
+        'type': 'string'
+      }
+    }
+  }
 
 };
 
