accbb25cff61b301f31b531105c4d92affb60075
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-06-24 21:44:21 +0700

    Added detailed users endpoint

--- app/users/users.routes.js
+++ app/users/users.routes.js
@@ -4,6 +4,7 @@ const express = require('express');
 const knex = require('../components/knex.js');
 const auth = require('../components/auth.js');
 const validators = require('./users.validators.js');
+const errors = require('http-errors');
 
 const router = express.Router();
 
@@ -19,6 +20,26 @@ router.get('/users', auth.isSupervisor, validators.listUsers, (req, res, next) =
     .pageAndSort(req.query.page, req.query.perPage, req.query.sort, ['username', 'nim', 'email', 'role', 'created_at', 'updated_at'])
     .then((result) => {
       return res.json(result);
+    })
+    .catch((err) => {
+      return next(err);
+    });
+});
+
+/**
+ * Get specific user information for the specified username.
+ * @name Get user info.
+ * @route {GET} /users/:username
+ */
+router.get('/users/:username', auth.isLoggedIn, (req, res, next) => {
+  return knex.select('username', 'nim', 'email', 'role', 'created_at', 'updated_at')
+    .from('users')
+    .where('username', req.params.username)
+    .first()
+    .then((user) => {
+      if (!user) return next(new errors.NotFound('User not found.'));
+      console.log(user);
+      return res.json(user);
     })
     .catch((err) => {
       return next(err);
