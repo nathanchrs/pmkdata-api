5f2a804d49d153470f640f930991c079e9741b75
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-06-28 15:41:07 +0700

    Added sample test using mocha and chai expect

---
+++ app/components/knex.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('Knex', function () {
+  describe('Test 1', function () {
+    it('should be true', function () {
+      expect(true).to.be.equal(true);
+    });
+  });
+});
