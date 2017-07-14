1c4b3fbcdaf52d642629929e54688015d132a6fd
ironlota <raydreww@gmail.com>
2017-07-14 15:14:10 +0700

    add users testing

--- app/users/users.queries.js
+++ app/users/users.queries.js
@@ -1,84 +1,83 @@
 'use strict';
 
-const knex = require('../components/knex.js');
+var knex = require('../components/knex.js');
 const errors = require('http-errors');
 const bcrypt = require('bcryptjs');
 
 const BCRYPT_STRENGTH = 8;
 
-function ensureOldPasswordIsCorrect (username, password) {
-  return knex.first('username', 'password').from('users').where('username', username)
-    .then(function (user) {
-      if (!user) throw new errors.Unauthorized('Wrong username or password.');
-      return bcrypt.compare(password, user.password);
-    })
-    .then((result) => {
-      if (!result) throw new errors.Unauthorized('Wrong username or password.');
-      return Promise.resolve();
-    });
+function ensureOldPasswordIsCorrect(username, password) {
+    return knex.first('username', 'password').from('users').where('username', username)
+        .then(function(user) {
+            if (!user) throw new errors.Unauthorized('Wrong username or password.');
+            return bcrypt.compare(password, user.password);
+        })
+        .then((result) => {
+            if (!result) throw new errors.Unauthorized('Wrong username or password.');
+            return Promise.resolve();
+        });
 }
 
 module.exports = {
+    listUsers: (search, page, perPage, sort) => {
+        return knex.select('username', 'nim', 'email', 'role', 'status', 'created_at', 'updated_at')
+            .from('users')
+            .search(search, ['username', 'nim', 'email'])
+            .pageAndSort(page, perPage, sort, ['username', 'nim', 'email', 'role', 'status', 'created_at', 'updated_at']);
+    },
 
-  listUsers: (search, page, perPage, sort) => {
-    return knex.select('username', 'nim', 'email', 'role', 'status', 'created_at', 'updated_at')
-      .from('users')
-      .search(search, ['username', 'nim', 'email'])
-      .pageAndSort(page, perPage, sort, ['username', 'nim', 'email', 'role', 'status', 'created_at', 'updated_at']);
-  },
+    createUser: (newUser) => {
+        let query = knex.select('username').from('users').where('username', newUser.username);
+        if (newUser.nim) query = query.orWhere('nim', newUser.nim);
 
-  createUser: (newUser) => {
-    let query = knex.select('username').from('users').where('username', newUser.username);
-    if (newUser.nim) query = query.orWhere('nim', newUser.nim);
+        return query.first()
+            .then((existingUsers) => {
+                if (existingUsers && existingUsers.length > 0) {
+                    if (existingUsers[0].username === newUser.username) {
+                        throw new errors.Conflict('Username already exists.');
+                    } else {
+                        throw new errors.Conflict('There is already a user for this NIM.');
+                    }
+                }
+                return bcrypt.hash(newUser.password, BCRYPT_STRENGTH);
+            })
+            .then((hash) => {
+                newUser.password = hash;
+                return knex('users').insert(newUser);
+            });
+    },
 
-    return query.first()
-      .then((existingUsers) => {
-        if (existingUsers && existingUsers.length > 0) {
-          if (existingUsers[0].username === newUser.username) {
-            throw new errors.Conflict('Username already exists.');
-          } else {
-            throw new errors.Conflict('There is already a user for this NIM.');
-          }
+    getUser: (username) => {
+        return knex.select('username', 'nim', 'email', 'role', 'status', 'created_at', 'updated_at')
+            .from('users')
+            .where('username', username)
+            .first();
+    },
+
+    updateUser: (username, userUpdates, requireOldPasswordCheck = true, oldPassword = '') => {
+        let promises = Promise.resolve();
+
+        if (userUpdates.password) {
+            if (requireOldPasswordCheck) {
+                promises = promises.then(() => {
+                    return ensureOldPasswordIsCorrect(oldPassword);
+                });
+            }
+
+            promises = promises.then(() => {
+                return bcrypt.hash(userUpdates.password, BCRYPT_STRENGTH);
+            });
         }
-        return bcrypt.hash(newUser.password, BCRYPT_STRENGTH);
-      })
-      .then((hash) => {
-        newUser.password = hash;
-        return knex('users').insert(newUser);
-      });
-  },
 
-  getUser: (username) => {
-    return knex.select('username', 'nim', 'email', 'role', 'status', 'created_at', 'updated_at')
-      .from('users')
-      .where('username', username)
-      .first();
-  },
+        return promises
+            .then((hash) => {
+                userUpdates.password = hash; // If hash is not computed, will result in undefined, which will be ignored.
+                return knex('users').update(userUpdates).where('username', username);
+            });
+    },
 
-  updateUser: (username, userUpdates, requireOldPasswordCheck = true, oldPassword = '') => {
-    let promises = Promise.resolve();
-
-    if (userUpdates.password) {
-      if (requireOldPasswordCheck) {
-        promises = promises.then(() => {
-          return ensureOldPasswordIsCorrect(oldPassword);
-        });
-      }
-
-      promises = promises.then(() => {
-        return bcrypt.hash(userUpdates.password, BCRYPT_STRENGTH);
-      });
+    deleteUser: (username) => {
+        return knex('users').delete().where('username', username);
     }
-
-    return promises
-      .then((hash) => {
-        userUpdates.password = hash; // If hash is not computed, will result in undefined, which will be ignored.
-        return knex('users').update(userUpdates).where('username', username);
-      });
-  },
-
-  deleteUser: (username) => {
-    return knex('users').delete().where('username', username);
-  }
 
 };
--- app/users/users.spec.js
+++ app/users/users.spec.js
@@ -2,10 +2,83 @@
 'use strict';
 
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 const sinonChai = require('sinon-chai');
+chai.use(chaiHttp);
 chai.use(sinonChai);
 const sinon = require('sinon');
 const expect = chai.expect;
+const should = chai.should();
+const knex = require('../components/knex');
+const routes = require('../app');
+const queries = require('./users.queries');
 
-const queries = require('./users.queries.js');
+describe('User handling', function() {
+    afterEach((done) => {
+        knex.migrate.rollback()
+            .then(() => {
+                knex.migrate.latest()
+                    .then(() => {
+                        knex.seed.run()
+                            .then(() => {
+                                done();
+                            })
+                    });
+            });
+    });
 
+    describe('new user', function() {
+        let createNewUser = {
+            "nim": 13515073,
+            "username": "raydreww",
+            "email": "raydreww@gmail.com",
+            "password": "hello123"
+        };
+
+        it('should create new user if req.user is not falsy', (done) => {
+            chai.request(routes).post('/users').send(createNewUser).end((err, res) => {
+                expect(res).to.have.status(201);
+                expect(res).to.be.a('object');
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.haveOwnProperty('created_at');
+                expect(res.body.username).to.equal('raydreww');
+                done();
+            });
+        });
+
+        it('should not get list of users if user is not supervisor or him/herself', (done) => {
+            chai.request(routes).post('/users').send(createNewUser).end((err, res) => {
+                chai.request(routes).get('/users/' + createNewUser.username).end((errfromget, resfromget) => {
+                    expect(resfromget).to.have.status(401);
+                    expect(resfromget.body.message).to.equal('Unauthorized');
+                    expect(resfromget.body.name).to.equal('UnauthorizedError');
+                    done();
+                    11
+                });
+            });
+        });
+
+        it('should not delete user if user is not supervisor', (done) => {
+            chai.request(routes).post('/users').send(createNewUser).end((err, res) => {
+                chai.request(routes).delete('/users/' + createNewUser.username).end((errfromdel, resfromdel) => {
+                    expect(resfromdel).to.have.status(401);
+                    expect(resfromdel.body.message).to.equal('Unauthorized');
+                    expect(resfromdel.body.name).to.equal('UnauthorizedError');
+                    done();
+                    11
+                });
+            });
+        });
+
+        it('should not edit user if user is not supervisor or not user him/herself', (done) => {
+            chai.request(routes).post('/users').send(createNewUser).end((err, res) => {
+                chai.request(routes).patch('/users/' + createNewUser.username).send({ nim: 13515074 }).end((errfromdel, resfromdel) => {
+                    expect(resfromdel).to.have.status(401);
+                    expect(resfromdel.body.message).to.equal('Unauthorized');
+                    expect(resfromdel.body.name).to.equal('UnauthorizedError');
+                    done();
+                });
+            });
+        });
+    });
+});
