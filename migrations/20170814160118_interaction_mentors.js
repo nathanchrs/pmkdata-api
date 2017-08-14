5b742dc852bac86a67d94693227298113c945494
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-08-14 16:15:09 +0700

    Added migration for interaction participants and mentors, removed non-production seeds

--- migrations/20170814160118_interaction_mentors.js
+++ migrations/20170814160118_interaction_mentors.js
@@ -1,20 +1,20 @@
 
 exports.up = (knex, Promise) => {
   return Promise.all([
-    knex.schema.createTable('interaction_participants', table => {
+    knex.schema.createTable('interaction_mentors', table => {
       table.increments('id').primary();
       table.integer('interaction_id').unsigned().notNullable();
-      table.integer('student_id').unsigned().notNullable();
+      table.integer('user_id').unsigned().notNullable();
       table.timestamps();
 
       // table.foreign('interaction_id').references('interactions.id').onDelete('RESTRICT');
-      // table.foreign('student_id').references('students.id').onDelete('RESTRICT');
+      // table.foreign('user_id').references('users.id').onDelete('RESTRICT');
     })
   ]);
 };
 
 exports.down = (knex, Promise) => {
   return Promise.all([
-    knex.schema.dropTable('interaction_participants')
+    knex.schema.dropTable('interaction_mentors')
   ]);
 };
--- seeds/1.users.js
+++ seeds/1.users.js
@@ -5,20 +5,11 @@ exports.seed = (knex, Promise) => {
       let users = [
         {
           username: 'admin',
-          nim: '12315123',
           email: 'admin@pmk.itb.ac.id',
           password: '$2a$08$QwNk.YBBpry2N09/LVudj.ZvVtkL.6JIYvGd8Y8MT9VQ3mMfHwF8S',
           role: 'admin',
           status: 'active'
-        }, // Password: admin
-        {
-          username: 'admin2',
-          nim: '13515123',
-          email: 'admin2@pmk.itb.ac.id',
-          password: '$2a$08$QwNk.YBBpry2N09/LVudj.ZvVtkL.6JIYvGd8Y8MT9VQ3mMfHwF8S',
-          role: 'admin',
-          status: 'active'
-        } // Password: admin, for testing foreign key
+        } // Password: admin
       ];
       return Promise.all([
         // Inserts seed entries
